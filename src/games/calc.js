d14159d383ac7abb955ea2e34036c1fd1b7aff3b
M2-cc <masksadmamatov@gmail.com>
2026-04-06 15:45:16 +0700

    1jijjj

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,29 +1,28 @@
-import runEngine from '../index.js';
+import runEngine from '../index.js'
 
-const description = 'What is the result of the expression?';
+const description = 'What is the result of the expression?'
 
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-    case '+': return num1 + num2;
-    case '-': return num1 - num2;
-    case '*': return num1 * num2;
-    default: throw new Error(`Unknown operator: ${operator}`);
+    case '+': return num1 + num2
+    case '-': return num1 - num2
+    case '*': return num1 * num2
+    default: throw new Error(`Unknown operator: ${operator}`)
   }
-};
+}
 
 const getRoundData = () => {
-  const num1 = getRandomNumber(1, 20);
-  const num2 = getRandomNumber(1, 20);
-  const operators = ['+', '-', '*'];
-  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  const num1 = getRandomNumber(1, 20)
+  const num2 = getRandomNumber(1, 20)
+  const operators = ['+', '-', '*']
+  const operator = operators[getRandomNumber(0, operators.length - 1)]
 
+  const question = `${num1} ${operator} ${num2}`
+  const correctAnswer = String(calculate(num1, num2, operator))
 
-  const question = `${num1} ${operator} ${num2}`;
-  const correctAnswer = String(calculate(num1, num2, operator));
+  return [question, correctAnswer]
+}
 
-  return [question, correctAnswer];
-};
-
-export default () => runEngine(description, getRoundData);
+export default () => runEngine(description, getRoundData)
